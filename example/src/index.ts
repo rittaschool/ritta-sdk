f91a36e57986ad093cf28fde1af5ce5bd3ebaa4b
raikasdev <fanipostiraikas@gmail.com>
2021-07-30 17:58:37 +0300

    example: fixes

--- example/src/index.ts
+++ example/src/index.ts
@@ -5,11 +5,10 @@ export default class ExampleModule extends RittaModule {
   constructor(sdk: Ritta) {
     super(sdk);
     this.sdk.logger.info(this.sdk.modules().listModules());
-    try {
-      this.sdk.database().model('User');
-    } catch (e) {
+    this.sdk.database().model('User')
+    .catch((e) => {
       this.sdk.logger.error(e);
       // Throws error because no permission!
-    }
+    });
   }
 }
